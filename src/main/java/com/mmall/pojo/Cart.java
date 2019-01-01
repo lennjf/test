697063d500ff851a7d016ad0471309f59505d0cc
njf <leonnjf@126.com>
2019-01-01 22:49:00 +0800

    project init commit

---
+++ src/main/java/com/mmall/pojo/Cart.java
@@ -0,0 +1,89 @@
+package com.mmall.pojo;
+
+import java.util.Date;
+
+public class Cart {
+    private Integer id;
+
+    private Integer uid;
+
+    private Integer productId;
+
+    private Integer quantity;
+
+    private Integer checked;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Cart(Integer id, Integer uid, Integer productId, Integer quantity, Integer checked, Date createTime, Date updateTime) {
+        this.id = id;
+        this.uid = uid;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.checked = checked;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public Cart() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getChecked() {
+        return checked;
+    }
+
+    public void setChecked(Integer checked) {
+        this.checked = checked;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
